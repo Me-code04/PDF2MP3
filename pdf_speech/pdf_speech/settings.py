459612d2b7728135c4430fd27e9ff1bd8d98ab7b
Suhrid Sadman Abrar <suhridabrarbd@outlook.com>
2025-07-22 12:31:35 +0530

    INSTALLED_APPS

--- pdf_speech/pdf_speech/settings.py
+++ pdf_speech/pdf_speech/settings.py
@@ -7,3 +7,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 ROOT_URLCONF = 'pdf_speech.urls'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
+
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+
+    'pdf_app',  # your custom app
+]
